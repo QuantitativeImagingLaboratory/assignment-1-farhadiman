179a5e1a8520c56f1aac93cb78e9a79c77b38336
farhadiman <farhadiman@gmail.com>
2017-10-02 20:01:08 -0700

    Update resample.py

--- resize/resample.py
+++ resize/resample.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+import cv2
 class resample:
 
     def resize(self, image, fx = None, fy = None, interpolation = None):
@@ -25,6 +28,27 @@ class resample:
 
         #Write your code for nearest neighbor interpolation here
 
+        # iterate over the entire image.
+
+
+
+        height, width, channels = image.shape
+        mx =int( width * float(fy))
+        my = int(height *float (fx))
+        size = my,mx, 3
+        m = np.zeros(size, dtype=np.uint8)
+        for py in range(0, mx):
+            for px in range(0, my):
+                 m [px,py]=   image [int(round(px/float(fx))),int(round(py/float(fy)))]
+
+
+
+
+
+
+        cv2.imshow('matrix', m)
+        cv2.waitKey(0)
+
         return image
 
 
@@ -38,5 +62,51 @@ class resample:
 
         # Write your code for bilinear interpolation here
 
-        return image
+        height, width, channels = image.shape
 
+
+        mx = int((width-3) * float(fx))
+        my = int((height-3) * float(fy))
+
+        size = mx, my, 3
+        m = np.zeros(size, dtype=np.uint8)
+
+
+
+
+        for py in range(0, my):
+            for px in range(0, mx):
+
+                q21x= math.ceil(px / float(fx))
+                q21y = math.floor(py / float(fy))
+
+                q22x = math.ceil(px / float(fx))
+                q22y = math.ceil(py / float(fy))
+
+                q11x = math.floor(px / float(fx))
+                q11y = math.floor(py / float(fy))
+
+                q12x = math.floor(px / float(fx))
+                q12y = math.ceil(py / float(fy))
+
+                if (q22x - q11x != 0) and (q22y - q11y != 0):
+                  ir1 = ((q22x - (px / float(fx))) * image[int(q11x), int(q11y)]) + (((px / float(fx)) - q11x) * image[int(q21x), int(q21y)])
+                  ir2 = ((q22x - (px / float(fx))) * image[int(q12x), int(q12y)]) + (((px / float(fx)) - q11x) * image[int(q22x), int(q22y)])
+                  fin = (((q22y - (py / float(fy)))) * ir1) + ((((py / float(fy)) - q11y)) * ir2)
+                  m[px, py] = fin
+
+                if (q22x - q11x == 0) and (q22y - q11y != 0):
+                  ir1 = ((q22y - (py / float(fy))) * image[int(q11x), int(q11y)]) + (((py / float(fy)) - q11y) * image[int(q21x), int(q21y)])
+                  m[px, py] = ir1
+
+                if (q22x - q11x != 0) and (q22y - q11y == 0):
+                  ir1 = ((q22x - (px / float(fx))) * image[int(q11x), int(q11y)]) + (((px / float(fx)) - q11x) * image[int(q21x), int(q21y)])
+                  m[px, py] = ir1
+
+                if (q22x - q11x == 0) and (q22y - q11y == 0):
+                  m[px, py] = image[int(q11x), int(q11y)]
+
+        cv2.imshow('matrix', m)
+        cv2.waitKey(0)
+
+        return image
